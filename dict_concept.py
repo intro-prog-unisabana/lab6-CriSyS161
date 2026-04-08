22e9d4138699e53ef7a97e7cff62cbad6bb43337
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-08 17:22:29 -0500

    V5

--- dict_concept.py
+++ dict_concept.py
@@ -1,7 +1,7 @@
 def get_config():
     dic = {"width": 480,
-                   "height": 480,
-                   "color_mode": "dark",
-                   "sensivity": 0.4}
+            "height": 480,
+            "color_mode": "dark",
+            "sensivity": 0.4}
     return dic
 print(get_config())
