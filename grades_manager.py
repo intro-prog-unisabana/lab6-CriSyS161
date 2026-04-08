75b7f24c7a9caf048d348c1b76e9fc5b67e9aef8
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-08 18:23:29 -0500

    V10

--- grades_manager.py
+++ grades_manager.py
@@ -9,4 +9,28 @@ def add_student(student_grades = None):
         entry = input("Enter subject and grade (or 'exit' to finish): ").strip()
         if entry.lower() == 'exit':
             break
-        subject, grade = entry.split()
+        subject, grade = entry.split(',')
+        subjects[subject.strip().title()] = float(grade.strip())
+    student_grades[name] = subjects
+    print(f"Student {name} succesfully added to the grades management system. \n")
+    return student_grades
+def get_students(student_grades, keys):
+    result = {}
+    search = {name.lower(): name for name in student_grades.keys()}
+    for key in keys:
+        clean_key = key.strip().lower()
+        if clean_key in search:
+            original_name = search[clean_key]
+            result[original_name] = student_grades[original_name]
+        else:
+            print(f"{key.strip().title()} not found")
+    return result
+def avg_by_student(student_grades, keys = None):
+    if keys is not None:
+        dict = get_students(student_grades, keys)
+    else:
+        dict = student_grades
+    for student, subjects in dict.items():
+        if subjects:
+            average = sum(subjects.values()) / len(subjects)
+            print(f"{student}: {round(average, 1)}")
