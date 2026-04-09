d70c70b831c1462ebd270784861efc0983271dcc
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-08 21:40:37 -0500

    V13

--- industrial_temperature.py
+++ industrial_temperature.py
@@ -1,8 +1,6 @@
-def trigger_alarm(temperatures, threshold = 80):
+def trigger_alarm(temperatures, threshold=80):
     triggered_sensors = []
     for sensor, temp in temperatures.items():
         if temp > threshold:
-            triggered_sensors.append()
+            triggered_sensors.append(sensor)
     return triggered_sensors
-
-
